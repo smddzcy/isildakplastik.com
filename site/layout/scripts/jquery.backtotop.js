1265d9278c7c036b8431220303e7b0a4d86d68af
Samed Duzcay <samedduzcay@gmail.com>
2019-03-20 17:35:32 +0300

    Product detail pages in Turkish

--- site/layout/scripts/jquery.backtotop.js
+++ site/layout/scripts/jquery.backtotop.js
@@ -1,21 +1,22 @@
-/*
-Template Name: Halice
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
-File: Back to Top JS
-*/
-
-jQuery("#backtotop").click(function () {
-    jQuery("body,html").animate({
+$("#backtotop").click(function () {
+    $("body,html").animate({
         scrollTop: 0
     }, 600);
 });
-jQuery(window).scroll(function () {
-    if (jQuery(window).scrollTop() > 150) {
-        jQuery("#backtotop").addClass("visible");
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 150) {
+        $("#backtotop").addClass("visible");
     } else {
-        jQuery("#backtotop").removeClass("visible");
+        $("#backtotop").removeClass("visible");
     }
 });
+
+$(document).ready(function() {
+    $('[data-href]').each(function(idx, el) {
+        $(el).addClass('cp');        
+        $(el).on('click', function() {
+            window.location.href = $(el).data('href');
+        });
+    });
+})
