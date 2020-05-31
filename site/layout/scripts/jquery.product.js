c04061fc534a9fd3ec51fa86addb87fa93e1f902
onurcskun <onurcskun@gmail.com>
2020-05-31 22:07:24 +0300

    v2 of landing

---
+++ site/layout/scripts/jquery.product.js
@@ -0,0 +1,22 @@
+const hash = window.location.hash.replace("#", "");
+showProduct(hash);
+
+function setProduct(hash) {
+  showProduct(hash);
+}
+
+function showProduct(h) {
+  console.log(h);
+  const products = $('.product');
+  if (h !== "") {
+    for (i = 0; i < products.length; i++) {
+      if (products[i].id === h) {
+        $(products[i]).removeClass("hidden");
+      } else {
+        $(products[i]).addClass("hidden");
+      }
+    }
+  } else {
+    $(products[0]).removeClass("hidden");
+  }
+}
